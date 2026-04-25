9ceca2bd27c210c3a3a33e2b6810b6ce7f554978
YesithTK <yesithszc@gmail.com>
2026-04-24 20:16:50 -0500

    Add UptimeCommand class extending UptimeCmd

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/UptimeCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class UptimeCommand extends UptimeCmd {
+    public UptimeCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
