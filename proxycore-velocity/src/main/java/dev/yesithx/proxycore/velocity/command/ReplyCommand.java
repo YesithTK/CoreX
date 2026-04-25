d746d123bd9cb543144566e985fa99a8c43d5ef3
YesithTK <yesithszc@gmail.com>
2026-04-24 20:13:10 -0500

    Add ReplyCommand class extending ReplyCmd

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/ReplyCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class ReplyCommand extends ReplyCmd {
+    public ReplyCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
