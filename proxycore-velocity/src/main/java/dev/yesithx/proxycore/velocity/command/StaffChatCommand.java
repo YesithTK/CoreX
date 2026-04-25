9f2bee41ab3e878523f2471d666096f9c2c319c9
YesithTK <yesithszc@gmail.com>
2026-04-24 20:15:01 -0500

    Add StaffChatCommand class for staff chat functionality

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/StaffChatCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class StaffChatCommand extends StaffChatCmd {
+    public StaffChatCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
