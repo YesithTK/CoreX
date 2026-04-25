1aa91935d6e0692f8e3ad43b9e3ffd57ccde387f
YesithTK <yesithszc@gmail.com>
2026-04-24 20:07:46 -0500

    Add GlistCommand class extending GlistCmd

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/GlistCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class GlistCommand extends GlistCmd {
+    public GlistCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
