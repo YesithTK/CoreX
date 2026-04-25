16820d12d5e725d51c588e74366cdcaf98e847bf
YesithTK <yesithszc@gmail.com>
2026-04-24 20:11:26 -0500

    Add PingCommand class for handling ping commands

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/PingCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class PingCommand extends PingCmd {
+    public PingCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
